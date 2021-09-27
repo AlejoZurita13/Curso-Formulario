b9df6863b6391e9f660a78d0293c2bdfbc8525b3
Alejo <alejozuritar@gmail.com>
2021-09-27 01:54:43 -0300

    Agregar archivos de proyecto.

---
+++ WindowsFormsContacts/BusinessLogicLayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsContacts
+{
+    public class BusinessLogicLayer
+    {
+        private DataAccessLayer _dataAccessLayer;
+
+        public BusinessLogicLayer()
+        {
+            _dataAccessLayer = new DataAccessLayer();
+        }
+
+        #region Save Contact
+        public Contact SaveContact(Contact contact)
+        {
+            if (contact.Id == 0)
+                _dataAccessLayer.InsertContact(contact);
+            else
+                _dataAccessLayer.UpdateContact(contact); 
+
+            return contact;
+        }
+        #endregion
+
+        #region Busqueda de Dato
+        public List<Contact> GetContacts(string searchText = null)
+        {
+            return _dataAccessLayer.GetContacts(searchText);
+        }
+        #endregion
+
+        #region Llamada a DeleteContact
+        public void DeleteContact(int id)
+        {
+            _dataAccessLayer.DeleteContact(id);
+        }
+        #endregion 
+    }
+}
---
+++ WindowsFormsContacts/ContactDetails.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsContacts
+{
+    public partial class ContactDetails : Form
+    {
+        private BusinessLogicLayer _businessLogicLayer;
+        private Contact _contact;
+        public ContactDetails()
+        {
+            InitializeComponent();
+            _businessLogicLayer = new BusinessLogicLayer();
+        }
+        #region Boton Cancel 
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region Boton Save
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveContact();
+            this.Close();
+            ((Form1)this.Owner).PopulateContacts();
+        }
+        private void SaveContact()
+        {
+            Contact contact = new Contact();
+            contact.FirstName = txtFirstName.Text;
+            contact.LastName = txtLastName.Text;
+            contact.Phone = txtPhone.Text;
+            contact.Address = txtAddress.Text;
+
+            contact.Id = _contact != null ? _contact.Id : 0;
+
+            _businessLogicLayer.SaveContact(contact);
+        }
+        #endregion
+
+        #region Cargar Contacto
+        public void LoadContact(Contact contact)
+        {
+            _contact = contact;
+            if (contact != null)
+            {
+                txtFirstName.Text = contact.FirstName;
+                txtLastName.Text = contact.LastName;
+                txtPhone.Text = contact.Phone;
+                txtAddress.Text = contact.Address;
+            }
+        }
+        #endregion
+
+        #region Limpiar datos
+        private void ClearForm()
+        {
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+        }
+        #endregion
+    }
+
+
+}
